69a72cd5984577f26cd803b55d338885e1262063
sahal6282 <muhammedsahalks@gmail.com>
2025-08-05 21:21:33 +0530

    last commit

---
+++ src/php-sca-test/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'test/php-sca',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '67ced816a284120a264db1a09831bd2ecff9f7cc',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.1.5',
+            'version' => '6.1.5.0',
+            'reference' => 'a8bf068f64a580302026e484ee29511f661b2ad3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'test/php-sca' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '67ced816a284120a264db1a09831bd2ecff9f7cc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
